07bd9a0966fa0e06c31993fca0ba4a80634130fe
a.denti@errevi.com <a.denti@errevi.com>
2022-04-12 17:51:52 +0200

    Removed Interval in favor of event scheduling

--- src/index.ts
+++ src/index.ts
@@ -4,27 +4,25 @@ import { integrations } from './integrations_list';
 
 import tdtrSync from './integrations/Trello_Todoist';
 
-// process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
 
 require('dotenv').config();
 
 try {
-  setInterval(() => {
-    process.stdout.write(`-- POLLING: ${new Date().toString()}\n`);
+  process.stdout.write(`-- POLLING: ${new Date().toString()}\n`);
 
-    integrations.forEach((integration) => {
-      const endpointsTypes: string[] = integration.endpoints.map((end) => end.type).sort();
+  integrations.forEach((integration) => {
+    const endpointsTypes: string[] = integration.endpoints.map((end) => end.type).sort();
 
-      if (endpointsTypes[0] === 'todoist') {
-        if (endpointsTypes[1] === 'trello') {
-          const todoist : any = integration.endpoints.find((end) => end.type === 'todoist');
-          const trello : any = integration.endpoints.find((end) => end.type === 'trello');
+    if (endpointsTypes[0] === 'todoist') {
+      if (endpointsTypes[1] === 'trello') {
+        const todoist : any = integration.endpoints.find((end) => end.type === 'todoist');
+        const trello : any = integration.endpoints.find((end) => end.type === 'trello');
 
-          tdtrSync(todoist, trello);
-        }
+        tdtrSync(todoist, trello);
       }
-    });
-  }, 10000);
+    }
+  });
 } catch (err: any) {
   process.stderr.write(`Index.js: ${err.toString()}`);
 }
